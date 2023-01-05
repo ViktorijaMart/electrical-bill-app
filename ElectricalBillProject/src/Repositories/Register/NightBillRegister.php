67261b2e52caeaf539572155f636230e64743830
LhunaCat <viktorijamartinaityte@gmail.com>
2023-01-05 17:02:39 +0200

    added bill register

---
+++ ElectricalBillProject/src/Repositories/Register/NightBillRegister.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types=1);
+
+namespace Vikto\ElectricalBillProject\Repositories\Register;
+
+class NightBillRegister implements BillRegisterInterface
+{
+
+    private const JSON_FILE_PATH = __DIR__ . '/../../Files/bills.json';
+
+    public function addToJson(array $newBill): void
+    {
+        $billsArray = $this->decodeJson();
+
+        $updatedBillsArray = array_push($billsArray, $newBill);
+
+        file_put_contents(self::JSON_FILE_PATH, json_encode($updatedBillsArray));
+    }
+
+    public function decodeJson(): array
+    {
+        return json_decode(file_get_contents(self::JSON_FILE_PATH), true);
+    }
+}
