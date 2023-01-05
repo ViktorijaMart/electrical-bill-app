2b0ada6cb38cac627bf5592aa3983d0af299a9b3
LhunaCat <viktorijamartinaityte@gmail.com>
2023-01-05 16:14:34 +0200

    added router

--- ElectricalBillProject/index.php
+++ ElectricalBillProject/index.php
@@ -3,3 +3,18 @@ declare(strict_types=1);
 
 require_once './vendor/autoload.php';
 
+use Vikto\ElectricalBillProject\Container\DIContainer;
+
+$request = str_replace('/PHP_practise_test/ElectricalBillProject', '', $_SERVER['REQUEST_URI']);
+
+// Load custom DI container
+$container = new DIContainer();
+
+// Use custom Router
+
+try {
+    $router = $container->get('Vikto\ElectricalBillProject\Framework\Router');
+    $router->process($request);
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
--- ElectricalBillProject/src/Framework/Router.php
+++ ElectricalBillProject/src/Framework/Router.php
@@ -5,5 +5,19 @@ namespace Vikto\ElectricalBillProject\Framework;
 
 class Router
 {
-
+    public function process(string $request)
+    {
+        switch ($request) {
+            case '':
+            case '/':
+                require __DIR__ . '/../../views/index.php';
+                break;
+            case 'electricalBills/unpaidBills':
+                require __DIR__ . '/../../views/electricalBills/unpaidBills.php';
+                break;
+            default:
+                http_response_code(404);
+                require __DIR__ . '/../../views/error/error.php';
+        }
+    }
 }
