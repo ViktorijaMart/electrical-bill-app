a5058e996eef50eadd9297e62f6c481ec95bf421
LhunaCat <viktorijamartinaityte@gmail.com>
2023-01-05 17:30:33 +0200

    added bill controller

--- ElectricalBillProject/src/Controllers/BillController.php
+++ ElectricalBillProject/src/Controllers/BillController.php
@@ -1,8 +1,33 @@
 <?php
 
-namespace Vikto\ElectricalBillProject\Controller;
+namespace Vikto\ElectricalBillProject\Controllers;
+
+use Vikto\ElectricalBillProject\Container\DIContainer;
 
 class BillController
 {
+    public function __construct(private readonly DIContainer $container)
+    {}
 
+    public function getUnpaidBills(array $newBill): void
+    {
+        $billRepository = $this->container->get('Vikto\ElectricalBillProject\Repositories\BillRepository');
+
+        // parasyti if, kad patikrinti, ar array not empty, jei not empty, kviesti register funkcija
+        if (!empty($newBill)) {
+            $this->register($newBill);
+        }
+
+        // gauti bill object
+        $unpaidBills = $billRepository->getUnpaidBills();
+
+        require __DIR__ . '/../../views/electricalBills/unpaidBills.php';
+    }
+
+    private function register(array $newBill): void
+    {
+        $dayBillRegister = $this->container->get('Vikto\ElectricalBillProject\Repositories\Register\DayBillRegister');
+
+        $dayBillRegister->addToJson($newBill);
+    }
 }
