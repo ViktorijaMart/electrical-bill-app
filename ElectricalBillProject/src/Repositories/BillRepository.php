f75b48bb44b3f70523d788affacefad317df7526
LhunaCat <viktorijamartinaityte@gmail.com>
2023-01-05 17:31:22 +0200

    added bill repository

--- ElectricalBillProject/src/Repositories/BillRepository.php
+++ ElectricalBillProject/src/Repositories/BillRepository.php
@@ -4,5 +4,15 @@ namespace Vikto\ElectricalBillProject\Repositories;
 
 class BillRepository
 {
+    private const JSON_FILE_PATH = __DIR__ . '/../Files/bills.json';
 
+    public function getUnpaidBills(): array
+    {
+        return $this->decodeJSON();
+    }
+
+    private function decodeJSON(): array
+    {
+        return json_decode(file_get_contents(self::JSON_FILE_PATH), true);
+    }
 }
