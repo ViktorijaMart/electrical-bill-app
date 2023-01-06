d1baa2eb3a52163826c02eb335be93873d432a59
LhunaCat <viktorijamartinaityte@gmail.com>
2023-01-06 20:07:53 +0200

    added html and css

--- ElectricalBillProject/views/index.php
+++ ElectricalBillProject/views/index.php
@@ -1,8 +1,95 @@
 <?php
 declare(strict_types=1);
+?>
 
-//use Vikto\ElectricalBillProject\Container\DIContainer;
-//
-//$container = new DIContainer();
+<!DOCTYPE html>
+<style>
+    * {
+    margin: 0;
+    padding: 0;
+    }
 
-echo 'Hello';
+    body {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    img {
+        height: 240px;
+    }
+
+    a {
+        display: inline-block;
+        color: black;
+        cursor: pointer;
+    }
+
+    a,
+    h2 {
+        font-size: 20px;
+    }
+
+    h2 {
+        margin-bottom: 16px;
+    }
+
+    input,
+    label,
+    select {
+        margin-bottom: 12px;
+    }
+
+    label {
+        font-size: 18px;
+    }
+
+    input,
+    select {
+        height: 20px;
+    }
+
+    button {
+        font-size: 18px;
+        padding: 4px;
+        border-color: #4b7722;
+        background-color: #9db987;
+    }
+
+    #payment,
+    #form {
+        border: 5px double black;
+        width: 40vw;
+        padding: 24px;
+        text-align: center;
+    }
+
+    #payment {
+        margin-bottom: 32px;
+    }
+</style>
+<html>
+    <body>
+        <img src="https://www.logodee.com/wp-content/uploads/2022/01/6.jpg">
+        <div id="payment">
+            <a href='electricalBills/unpaidBills'>GO TO PAYMENT</a>
+        </div>
+        <div id="form">
+            <h2>ADD NEW BILL</h2>
+            <form method="post" action="electricalBills/unpaidBills">
+                <label for="usedKwh">Used kWh total:</label> <br/>
+                <input type="number" name="usedKwh"> <br/>
+                <label for="priceOfOneKwh">Price of 1 kWh (&euro;):</label> <br/>
+                <input type="number" step="0.01" name="priceOfOneKwh"> <br/>
+                <label for="tariff">Select tariff</label> <br/>
+                <select name="tariff">
+                    <option value="daytime">Daytime</option>
+                    <option value="nighttime">Nighttime</option>
+                </select> <br/>
+                <label for="month">Enter the month you want to pay for:</label> <br/>
+                <input type="month" name="month"> <br/>
+                <button type="submit">Calculate price</button>
+            </form>
+        </div>
+    </body>
+</html>
